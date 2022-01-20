323f1412bb4aefad4b182d192c20b085df8b681b
fusbudget <andrewsanser@gmail.com>
2022-01-20 15:35:34 +0100

    add region search

--- front/js/script.js
+++ front/js/script.js
@@ -5,9 +5,31 @@ document.addEventListener("DOMContentLoaded", function() {
 async function startProgram() {
     // form url according to user's choice (country name or capital)
     const form = document.querySelector('form');
+    // add events to the radio inputs 
+    const radioInputs = document.querySelectorAll('input[type=radio]');
+    radioInputs.forEach((i)=>{
+        i.addEventListener('click', function(){
+            if (i.value === 'region') {
+                document.querySelector('#selectGroup').style.display = 'block';
+                document.querySelector('input#country').disabled = true;
+            } else {
+                document.querySelector('#selectGroup').style.display = 'none';
+                document.querySelector('input#country').disabled = false;
+            }
+        });
+    });
+
+
     form.addEventListener("submit", function(e) {
         e.preventDefault();
-        displayCountry();
+        let typeOfSearch = getSearchType();
+        if (typeOfSearch === 'region') {
+            getRegion();
+            console.log('getRegion');
+        } else {
+            displayCountry();
+            console.log('displayCountry');
+        }
     });
 }
 
@@ -18,15 +40,20 @@ async function getCountry(url) {
 } 
 
 async function makeSearch() {
-    let form = document.querySelector('form');
-    let data = new FormData(form);
-    let searchChoice = data.getAll('search-param')[0];
-    console.log(searchChoice);
+    let searchType = getSearchType();
     let searchInput = document.querySelector('#country').value.toLowerCase();
-    let url = `https://restcountries.com/v3.1/${searchChoice}/${searchInput}`;
+    let url = `https://restcountries.com/v3.1/${searchType}/${searchInput}`;
     // get county information
     const country = await getCountry(url);
+    console.log(country);
     return country;
+}
+
+function getSearchType() {
+    let form = document.querySelector('form');
+    let data = new FormData(form);
+    // return value from input[type=radio, name=search-param] 
+    return data.getAll('search-param')[0];
 }
 
 async function displayCountry() {
@@ -40,4 +67,12 @@ async function displayCountry() {
                     <span>Continent: ' + country[0].continents[0] + '</span><br><br>';
     li.insertAdjacentHTML('afterbegin', content);
     document.querySelector('#ulWithCountries').appendChild(li);
+}
+
+async function getRegion() {
+    let region = document.querySelector('select').value;
+    let countries = await fetch(`https://restcountries.com/v3.1/region/${region}`);
+    countries = await countries.json();
+    console.log(countries);
+    
 }
